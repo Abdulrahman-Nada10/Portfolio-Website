c1d751d1e7c3ade4be9e4b70584c339e78d80364
Abdulrahman-Nada10 <abdulrahmannada10@gmail.com>
2025-07-16 12:03:23 +0300

    social

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,9 +2,9 @@ import Link  from "next/link"
 import { FaLinkedinIn , FaFacebook , FaInstagram} from "react-icons/fa";
 const Social = () => {
     const socials = [
-        {icon : <FaFacebook/> , path: "https://www.facebook.com/share/14E5nGsK3ab/" },
-        {icon : <FaLinkedinIn/> , path: "https://www.linkedin.com/in/abdulrahman-nada-52487a32b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
-        {icon : <FaInstagram/> , path: "https://www.instagram.com/lo_ki65?igsh=M2Fha3cyN2tuMjNi" }
+        {icon : <FaFacebook/> , path: "https://www.facebook.com/profile.php?id=61563884250861" },
+        {icon : <FaLinkedinIn/> , path: "https://www.linkedin.com/company/105264662/admin/page-posts/published/?shareMsgArgs=null" },
+        {icon : <FaInstagram/> , path: "https://www.instagram.com/arabiansystemsacademy/?__pwa=1" }
     ]
   return (
     <div className="flex gap-6 text-right">
